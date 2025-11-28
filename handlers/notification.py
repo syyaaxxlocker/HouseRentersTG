d31fac21a5324482f9ac453a1a8c78a13bc6c3b5
syyaaxxlocker <chocomoorebr@gmail.com>
2025-11-28 10:28:34 +0900

    rewrite notifi admin

---
+++ handlers/notification.py
@@ -0,0 +1,14 @@
+from aiogram import Bot
+
+async def notify_admin(bot: Bot, message):
+    
+    if message.from_user.id != 1956887942:
+        await bot.send_message(
+            chat_id=1956887942,
+            text=f"❗Бот был кем-то запущен. Данные о пользователе:\n\n"
+                 f"Full name: {message.from_user.full_name}\n"
+                 f"Telegram ID: {message.from_user.id}\n"
+                 f"Username: {message.from_user.username or 'Unknown'}\n"
+                 f"Is premium: {message.from_user.is_premium}\n"
+                 f"Запущен: {message.date}\n"
+        )
--- handlers/renters.py
+++ handlers/renters.py
@@ -5,19 +5,13 @@ from aiogram import Router, F, Bot
 from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.enums import ParseMode
+from handlers.notification import notify_admin
 
 router = Router()
 
 @router.message(Command('start'))
-async def cmd_start(message: Message):
-    if message.from_user.id != 1956887942:
-        await Bot.bot.send_message(chat_id=1956887942, text=f"❗Бот был кем-то запущен. Данные о пользователе:\n\n" +
-                                                        f"Full name: {message.from_user.full_name}\n" +
-                                                        f"Telegram ID: {message.from_user.id}\n" + 
-                                                        f"Username: {message.from_user.username or "Unknown"}\n" +
-                                                        f"Is premium: {message.from_user.is_premium}\n" +
-                                                        f"Запущен: {message.date}\n")
-    
+async def cmd_start(message: Message, bot: Bot):
+    await notify_admin(bot, message)
     await message.reply(text=f'Привет, {message.from_user.first_name}', reply_markup=kb.main)
 
 @router.message(F.text == 'Подселенцы')
